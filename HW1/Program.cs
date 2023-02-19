ac736da90b350b57341ba35b7b5d14b0d11a5c18
Tanya Panther <tanya.panther@mail.ru>
2023-02-20 05:34:55 +0900

    add HW1

---
+++ HW1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+int number = ReadInt("введите трёхзначное число: ");
+int amount = number.ToString().Length;
+
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("Это не трёхзначное число");
+}
+else
+{
+    Console.WriteLine(InCenter(number));
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int InCenter(int a)
+{
+    int result = ((a/10)% 10);
+    return result;
+}
