0404420e95a07c7f505f1a9ac866e9ab809d5a68
Tanya Panther <tanya.panther@mail.ru>
2023-02-20 07:07:50 +0900

    add FromSite

---
+++ FromSite/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+int n = Convert.ToInt32(Console.ReadLine());
+int max = -1, max1 = -1;
+while (n != 0)
+{
+  n = Convert.ToInt32(Console.ReadLine());
+  if(n >= max)
+{
+    max1 = max;
+    max = n;
+}
+else
+if(n > max1)
+{
+    max1 = n;
+}
+}
+Console.WriteLine(max1);
