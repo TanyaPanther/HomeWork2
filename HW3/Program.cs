b8896ca29626205397fa7b6edaf9984ed9375c28
Tanya Panther <tanya.panther@mail.ru>
2023-02-20 05:35:35 +0900

    add HW3

---
+++ HW3/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+
+int dayNumber = ReadInt("Введите день недели от 1 до 7: ");
+Console.WriteLine(Holiday(dayNumber));
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+string Holiday(int a)
+{
+    if (a > 0 && a < 8)
+    {
+        if (a == 7 || a == 6)
+        {
+            Console.Write("Выходной");
+        }
+        else
+        {
+            Console.Write("Не выходной");
+        }
+    }
+    else
+    {
+        Console.Write("Введите число от 1 до 7 обозначающий");
+    }
+    return " день.";
+}
